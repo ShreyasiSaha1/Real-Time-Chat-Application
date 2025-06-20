9d8215c43d83736c66940dc0f3af0aa375d02e2e
Shreyasi Saha <shreyasisaha74@gmail.com>
2025-06-21 04:05:51 +0530

    Some files changed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,16 +1,16 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import msgRoutes from "./routes/msg.route.js";
 import cors from "cors";
 import path from "path";
-import {app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "2mb" }));
@@ -25,15 +25,20 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/msg", msgRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("*", (req, res) =>{
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  })
+  });
 }
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectDB();
-});
+connectDB()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database:", err);
+  });
